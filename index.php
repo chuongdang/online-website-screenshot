4be31e3d8a9d8a806a89f51457fe283fd7bcb4d7
Chuong Dang <chuong.php@gmail.com>
2014-03-27 23:17:49 +0700

    Fix for linux

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ if (!empty($_POST['url'])) {
     $width = $_POST['width'];
     $height = $_POST['height'];
     $path = dirname(__FILE__) . '/captured';
+    $phantomPath = '/usr/bin/phantomjs';
+    $userAgent = getenv("HTTP_USER_AGENT");
+    if (strpos($userAgent, 'Mac') !== FALSE) {
+        $phantomPath = '/usr/local/bin/phantomjs';
+    }
     $command = sprintf(
-        '/usr/local/bin/phantomjs util/capture.js %s %s %s %s',
+        '%s util/capture.js %s %s %s %s',
+        $phantomPath,
         $url,
         $path,
         $width,
